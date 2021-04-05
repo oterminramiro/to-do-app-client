bb918dd0cc0e3ce8bad461f22da2403a7a3aa11a
ramiro <oterminramiro@gmail.com>
2021-04-05 16:19:00 -0300

    update 404 page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,19 +1,44 @@
 <template>
-  <div class="container">
-    <header class="jumbotron">
-      <h3>Not Found</h3>
-    </header>
-  </div>
+	<ion-grid>
+		<ion-row class="ion-align-items-center">
+			<ion-col class="ion-align-self-center ion-text-center">
+				<ion-text color="dark">
+					<h1>404! Not found</h1>
+				</ion-text>
+				<ion-button href="/" color="dark" shape="round">Go home</ion-button>
+			</ion-col>
+		</ion-row>
+	</ion-grid>
+
 </template>
+
+<style>
+	ion-grid{
+		height: 100%;
+	}
+	ion-row{
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		height: 100%;
+	}
+</style>
 
 <script>
 
+import {
+	IonButton,IonCol, IonGrid, IonRow
+} from '@ionic/vue';
+
 export default {
-  name: 'NotFound',
-  data() {
-    return {
-      content: ''
-    };
-  },
+	name: 'NotFound',
+	data() {
+		return {
+			content: ''
+		};
+	},
+	components: {
+		IonButton, IonCol, IonGrid, IonRow
+	}
 };
 </script>
