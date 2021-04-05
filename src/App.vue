43f29f8e69f4c30b8022624daf512eb075b31d1b
ramiro <oterminramiro@gmail.com>
2021-04-05 15:57:41 -0300

    remove extra views

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,5 @@
 <template>
 	<ion-page>
-		<ion-header>
-			<ion-toolbar>
-				<ion-title>Home</ion-title>
-			</ion-toolbar>
-		</ion-header>
 		<ion-content class="ion-padding">
 			<router-view />
 		</ion-content>
@@ -14,20 +9,14 @@
 <script>
 	import {
 		IonContent,
-		IonHeader,
-		IonPage,
-		IonTitle,
-		IonToolbar
+		IonPage
 	} from '@ionic/vue';
 	import { defineComponent } from 'vue';
 
 	export default defineComponent({
 		components: {
 			IonContent,
-			IonHeader,
-			IonPage,
-			IonTitle,
-			IonToolbar
+			IonPage
 		}
 	});
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,8 @@
 import Vue from 'vue';
 import { createRouter, createWebHistory } from '@ionic/vue-router'
 
-import Home from '../views/Home.vue';
 import Login from '../views/Login.vue';
 import Register from '../views/Register.vue';
-import Profile from '../views/Profile.vue';
 import Task from '../views/Task.vue';
 import NewTask from '../views/NewTask.vue';
 import NotFound from '../views/NotFound.vue';
@@ -16,7 +14,7 @@ const router = createRouter({
 	routes: [
 		{
 			path: '/',
-			component: Home
+			component: Task
 		},
 		{
 			path: '/register',
@@ -25,14 +23,6 @@ const router = createRouter({
 		{
 			path: '/login',
 			component: Login
-		},
-		{
-			path: '/profile',
-			component: Profile
-		},
-		{
-			path: '/tasks',
-			component: Task
 		},
 		{
 			path: '/new_tasks',
